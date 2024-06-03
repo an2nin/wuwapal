e16e30b4153f939347fa9373f31c41b1939d3d08
Md Antonin Islam <antonin.islam@gmail.com>
2024-06-04 04:59:02 +0600

    reverted redux-persist ssg expriment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -12,25 +12,24 @@ import { store } from "@/redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
 
     if (router.pathname === "/_error") return <Component {...pageProps} />;
-
-    // Render nothing on the server and during initial hydration
-    if (!isMounted) return null;
+    if (router.pathname.startsWith("/convene"))
+        return (
+            <Layout>
+                <Provider store={store}>
+                    <PersistGate loading={null} persistor={persistor}>
+                        <Toaster />
+                        <Component {...pageProps} />
+                    </PersistGate>
+                </Provider>
+            </Layout>
+        );
 
     return (
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <Layout>
-                    <Toaster />
-                    <Component {...pageProps} />
-                </Layout>
-            </PersistGate>
-        </Provider>
+        <Layout>
+            <Toaster />
+            <Component {...pageProps} />
+        </Layout>
     );
 }

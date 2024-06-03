c05b97a9d0903cc868c3d4f8b6b801af9127d4e0
Md Antonin Islam <antonin.islam@gmail.com>
2024-06-04 00:42:50 +0600

    fixes header

--- src/components/layouts/HeaderNew.tsx
+++ src/components/layouts/HeaderNew.tsx
@@ -49,57 +49,31 @@ export default function HeaderNew() {
                     </SheetTrigger>
                     <SheetContent side="left" className="flex flex-col">
                         <nav className="grid gap-2 text-lg font-medium">
-                            <Link
-                                href="#"
-                                className="flex items-center gap-2 text-lg font-semibold"
-                            >
-                                <Package2 className="h-6 w-6" />
-                                <span className="sr-only">Acme Inc</span>
-                            </Link>
-                            <Link
-                                href="#"
-                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <Home className="h-5 w-5" />
-                                Dashboard
-                            </Link>
-                            <Link
-                                href="#"
-                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-                            >
-                                <ShoppingCart className="h-5 w-5" />
-                                Orders
-                                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                                    6
-                                </Badge>
-                            </Link>
-                            <Link
-                                href="#"
-                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <Package className="h-5 w-5" />
-                                Products
-                            </Link>
-                            <Link
-                                href="#"
-                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <Users className="h-5 w-5" />
-                                Customers
-                            </Link>
-                            <Link
-                                href="#"
-                                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <LineChart className="h-5 w-5" />
-                                Analytics
-                            </Link>
+                            <div className="text-2xl text-primary font-bold">
+                                WuWaPal
+                            </div>
+                            {navs.map((nav, idx) => (
+                                <Link
+                                    key={idx}
+                                    href={nav.href}
+                                    className={`flex justify-start items-center gap-3 rounded-lg p-3 transition-all ${
+                                        currentActiveNav === nav.href
+                                            ? "bg-accent text-accent-foreground "
+                                            : "text-muted-foreground hover:text-primary"
+                                    }`}
+                                >
+                                    {nav.icon}
+                                    {nav.title}
+                                </Link>
+                            ))}
                         </nav>
                     </SheetContent>
                 </Sheet>
                 <div className="hidden md:block">
                     <div className="w-full flex items-center justify-between px-5">
-                        <div className="text-2xl text-primary font-bold">WuWaPal</div>
+                        <div className="text-2xl text-primary font-bold">
+                            WuWaPal
+                        </div>
                         <div className="flex items-center gap-5 justify-center">
                             {navs.map((nav, idx) => (
                                 <Link

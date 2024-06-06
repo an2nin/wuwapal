7cc9c746be34fe047ed0f6fd15f15fb7b6454eb6
Md Antonin Islam <antonin.islam@gmail.com>
2024-06-07 05:22:06 +0600

    deletes reset file

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Settings() {
   return (
         <div className='flex flex-col gap-5'>
-            
+            Coming Soon!!!
         </div>
   )
 }

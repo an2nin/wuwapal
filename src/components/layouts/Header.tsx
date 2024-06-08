3f136430a23e552c7167b85dd1a9d444a5b71fae
Md Antonin Islam <antonin.islam@gmail.com>
2024-06-08 16:15:36 +0600

    fixes double header issue

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -71,7 +71,9 @@ export default function Header() {
                         </SheetContent>
                     </Sheet>
 
-                    <BrandLogo />
+                    <div className="md:hidden">
+                        <BrandLogo />
+                    </div>
                     <div></div>
                 </div>
 

93cc8a82843c9e78df6848083e8ff489907cb7c7
Antonin Islam <antonin.islam.dev@gmail.com>
2024-09-01 06:15:15 +0600

    update combatants in resonator battle

--- src/app/(home)/_components/ResonatorBattle.tsx
+++ src/app/(home)/_components/ResonatorBattle.tsx
@@ -7,7 +7,7 @@ import { useGlobalStatsQuery } from "@/redux/services/banner";
 import MovingBorder from "@/app/_components/ui/moving-border";
 import { COMBINED } from "@/shared/combined";
 
-const combatants = ["Changli", "Jinhsi"];
+const combatants = ["Jinhsi", "Zhezhi"];
 
 export default function ResonatorBattle() {
     const router = useRouter();
--- src/redux/api/endpoints.ts
+++ src/redux/api/endpoints.ts
@@ -1,3 +1,4 @@
 export const GM_SERVER_ENDPOINT = "https://gmserver-api.aki-game2.net/gacha/record/query";
 export const GLOBAL_STAT_GIST = "https://gist.githubusercontent.com/antonin686/3f10d0695f68210dd7dcce856cb557a1/raw/global_pull_stat.json"
+// export const GLOBAL_STAT_GIST = "https://gist.githubusercontent.com/antonin686/7103c5083e8632f6092486cc0ad71e0e/raw/c572e206d1b3ca99e3a73991d2ad0bb74160f2c0/test_global_script.json"
 export const API_SERVER_ENDPOINT= process.env.NEXT_PUBLIC_API_SERVER

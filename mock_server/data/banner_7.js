c2cde3e8710c5926a578ebb149e798fdf4167339
Antonin Islam <antonin.islam.dev@gmail.com>
2024-11-20 11:34:03 +0600

    add mock server data

---
+++ mock_server/data/banner_7.js
@@ -0,0 +1,25 @@
+const queryParams = {
+    cardPoolId: "0ed447ce1916fba28061c6b71467c401",
+    cardPoolType: 7,
+    playerId: "900279102",
+    serverId: "10cd7254d57e58ae560b15d51e34b4c8",
+    languageCode: "en",
+    recordId: "daa85481323563d3135719ad112bf949"
+};
+
+
+export const response = {
+    code: 0,
+    message: "success",
+    data: [
+        {
+            cardPoolType: "7",
+            resourceId: 1503,
+            qualityLevel: 5,
+            resourceType: "Resonator",
+            name: "Verina",
+            count: 1,
+            time: "2024-05-26 12:25:38"
+        }
+    ]
+}

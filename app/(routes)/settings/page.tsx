5c2e499437f3b77fd68f711f850f5acafd3cafe6
Antonin Islam <an2nin.dev@gmail.com>
2026-01-01 02:07:19 +0600

    feat(settings): implement tabbed interface for settings page to separate basic and advanced options

--- app/(routes)/settings/page.tsx
+++ app/(routes)/settings/page.tsx
@@ -1,13 +1,26 @@
 import AccountManager from '@/features/account-manager';
 import AdvancedSettings from '@/features/advanced-settings';
 import Backups from '@/features/backups';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/shared/components/ui/tabs';
 
 export default function SettingPage() {
   return (
-    <div className="flex flex-col lg:gap-6 gap-4">
-      <AccountManager />
-      <Backups />
-      <AdvancedSettings />
+    <div>
+      <Tabs defaultValue="basic">
+        <TabsList className="mb-6">
+          <TabsTrigger value="basic">Basic</TabsTrigger>
+          <TabsTrigger value="advanced">Advanced</TabsTrigger>
+        </TabsList>
+        <TabsContent value="basic">
+          <div className="flex flex-col gap-4 lg:gap-6">
+            <AccountManager />
+            <Backups />
+          </div>
+        </TabsContent>
+        <TabsContent value="advanced">
+          <AdvancedSettings />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
